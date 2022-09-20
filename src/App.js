afc95cffc40809c84280f44df6d6503d58c8adc8
Lance Gee <Lancegee.apps@gmail.com>
2022-09-20 13:50:47 -0600

    fixed routing issues to blog

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Blog1 from "./pages/Blog1";
-import Blog2 from "./pages/Blog2";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Blog1 from './pages/Blog1';
+import Blog2 from './pages/Blog2';
 
 function App() {
   useEffect(() => {
-    document.title = "My Blogs";
+    document.title = 'My Blogs';
   }, []);
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Blog1 />}></Route>
+        <Route path="/myFirstBlog" element={<Blog1 />}></Route>
         <Route path="/blog2" element={<Blog2 />}></Route>
       </Routes>
     </Router>
